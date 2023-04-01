868480159b5b68a327b56186533d91f89f11a8d2
hadhakautsar <112202006431@mhs.dinus.ac.id>
2023-04-01 13:01:23 +0700

    Commit Section 20

---
+++ 20_ORM & Code Structure (MVC)/Praktikum/section-20/controller/user_controller.go
@@ -0,0 +1,88 @@
+package controller
+
+import (
+	"net/http"
+	"strconv"
+
+	"section-20/config"
+	"section-20/model"
+
+	"github.com/labstack/echo/v4"
+)
+
+// get all users
+func GetUsersController(c echo.Context) error {
+	var users []model.User
+
+	if err := config.DB.Find(&users).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success get all users",
+		"users":   users,
+	})
+}
+
+// get user by id
+func GetUserController(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	var user model.User
+
+	if err := config.DB.First(&user, id).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success get user by id",
+		"user":    user,
+	})
+}
+
+// create new user
+func CreateUserController(c echo.Context) error {
+	user := model.User{}
+	c.Bind(&user)
+
+	if err := config.DB.Save(&user).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success create new user",
+		"user":    user,
+	})
+}
+
+// delete user by id
+func DeleteUserController(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	if err := config.DB.Delete(&model.User{}, id).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success delete user by id",
+	})
+}
+
+// update user by id
+func UpdateUserController(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	var user model.User
+
+	if err := config.DB.First(&user, id).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	c.Bind(&user)
+
+	if err := config.DB.Save(&user).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success",
+		"user":    user,
+	})
+
+}
